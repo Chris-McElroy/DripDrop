70307cb278b2d7fe1757f85da9afb473d3dc6b71
ChrisMcElroy <csmcelroy@me.com>
2022-12-15 19:43:30 +0000

    Initial Commit

---
+++ DripDrop/DripDropApp.swift
@@ -0,0 +1,17 @@
+//
+//  DripDropApp.swift
+//  DripDrop
+//
+//  Created by Chris McElroy on 12/15/22.
+//
+
+import SwiftUI
+
+@main
+struct DripDropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
